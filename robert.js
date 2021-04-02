4a11ddc5c311dc9d08a8e2df06cd13b0b8427355
disehn12 <65562620+disehn12@users.noreply.github.com>
2021-04-02 15:39:35 -0400

    Resolution Readout
    Reads out Resolution

---
+++ robert.js
@@ -0,0 +1,9 @@
+// read out the client width and height and print to screen
+var w = window.innerWidth
+    document.documentElement.clientWidth
+    document.body.clientWidth;
+var h = window.innerHeight
+    document.documentElement.clientHeight
+    document.body.clientHeight;
+var x = document.getElementById("k");
+x.innerHTML = w + "x" + h;
