a347fa43ff49f1a39efb811aef78a3bf9a3adc6b
disehn12 <65562620+disehn12@users.noreply.github.com>
2021-04-05 00:06:43 -0400

    added the area man
    why? i don't know why

--- robert.js
+++ robert.js
@@ -15,3 +15,16 @@ document.getElementById("lang").innerHTML = navigator.language;
 document.getElementById("ua").innerHTML = navigator.userAgent;
 // read out platform and print to screen
 document.getElementById("ap").innerHTML = navigator.appName;
+/* Area Man JS Edition */
+console.log("*****GREETINGS FROM THE AREA MAN*****");
+let radius = 5;
+let areaC = (Math.PI) * Math.pow(radius, 2);
+console.log("The area of the circle is " + areaC);
+let length = 5;
+let width = 9;
+let areaR = length * width;
+console.log("The area of the rectangle is " + areaR);
+let base = 5;
+let height = 11;
+let areaT = base * height * .5;
+console.log("The area of the triangle is " + areaT);
