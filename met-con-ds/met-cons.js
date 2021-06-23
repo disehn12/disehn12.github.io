f9d872c0c6abeccee76931d0200833bcb3ee327e
Daniel Sehn <Daniel.Sehn@thearcolc.org>
2021-06-23 10:00:09 -0400

    New things coming soon!

---
+++ met-con-ds/met-cons.js
@@ -0,0 +1,33 @@
+function centimeters2Inches (centimeters) {
+  return centimeters * 0.3937;
+}
+function inches2Centimeters (inches) {
+  return inches * 2.540;
+}
+function kilometers2Miles (kilometers) {
+  return kilometers * 0.621;
+}
+function miles2Kilometers (miles) {
+  return miles * 1.609;
+}
+function meters2Feet (meters) {
+  return meters * 3.281;
+}
+function feet2Meters (feet) {
+  return feet * 0.3048;
+}
+function feet2Inches (feet) {
+  return feet * 12.00;
+}
+function feet2Yards (feet) {
+  return feet * 0.3333;
+}
+function meters2Yards (meters) {
+  return meters * 1.093;
+}
+function miles2Feet (miles) {
+  return miles * 5280.00;
+}
+function miles2Yards (miles) {
+  return miles * 1760.00;
+}
